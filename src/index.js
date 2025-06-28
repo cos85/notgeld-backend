c7dbe48c5c51a90966347773e281826b2ea0e3c2
Cosimo <cosimo.allegretti@gmail.com>
2025-06-28 22:29:55 +0200

    Primo commit - backend Notgeld

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Notgeld API pronta!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server in ascolto sulla porta ${PORT}`);
+});
