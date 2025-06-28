6e889c03a0daf1a3d1d8197cd4e72aadcc8f0a50
Cosimo <cosimo.allegretti@gmail.com>
2025-06-28 23:15:40 +0200

    Frontend HTML per upload immagini

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import notgeldRouter from './routes/notgeld.js';
 import uploadRouter from './routes/upload.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 
 dotenv.config();
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use('/upload', uploadRouter);
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/notgeld', notgeldRouter);
 
